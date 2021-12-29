81f01e04d4c1ed26dccaa792fc580bbf83a5acd2
zhongsheng <tianzhongsheng168@gmail.com>
2021-12-29 18:07:14 +0800

    调整抓取

--- dy.go
+++ dy.go
@@ -310,7 +310,7 @@ func GetHttpHtmlContent(url string, selector string, sel interface{}) (string, e
 }
 
 func main() {
-	wg.Add(10)
+	wg.Add(2)
 	list := []string{
 		"https://www.domp4.cc/list/1-%v.html",
 		"https://www.domp4.cc/list/2-%v.html",
