ef8affb333065664d4638a87079878b687f2bf5c
zhongsheng <tianzhongsheng168@gmail.com>
2022-01-22 20:17:01 +0800

    部署

--- util/crawl.go
+++ util/crawl.go
@@ -132,7 +132,6 @@ func GetContentNewAll(dy *model.Dy) model.Dy {
 	doc.Find(".director").NextUntilMatcher(goquery.Single(".text .tag ")).Each(func(i int, s *goquery.Selection) {
 		t_em := strings.TrimSpace(s.Find("em").Text())
 		t_em = strings.ReplaceAll(t_em, "：", "")
-		fmt.Println("######", t_em, "-------", s.Find("span").Text())
 		if t_em == "地区" {
 			dy.Area = strings.TrimSpace(s.Find("span").Text())
 		}
