fbb43deaf09cd3e28cf9b344f100351ba2ab670c
zhongsheng <tianzhongsheng168@gmail.com>
2022-01-27 14:32:52 +0800

    修复

---
+++ import_image/importImage.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crwal/db"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("begin", time.Now().Format("2006-01-02 15:04:05"))
+
+	var path = flag.String("path", "", "image path")
+	flag.Parse()
+	fmt.Println("path:", *path)
+	if *path == "" {
+		fmt.Println("path 不能为空")
+		os.Exit(1)
+	}
+
+	id := db.SaveImage(*path)
+	fmt.Println("mongo gridfs id: ", id)
+
+	fmt.Println("sucess")
+
+	fmt.Println("end", time.Now().Format("2006-01-02 15:04:05"))
+
+}
