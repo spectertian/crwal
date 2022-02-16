563876f2335ba0d45cfdc12082357bca30bf7e71
zhongsheng <tianzhongsheng168@gmail.com>
2022-02-16 18:01:53 +0800

    视频采集

--- model/vod.go
+++ model/vod.go
@@ -5,178 +5,91 @@ import (
 	"time"
 )
 
-type WVods struct {
-	VodId            string
-	TypeId           string
-	TypeIdParent     string
-	GroupId          string
-	VodName          string
-	VodSub           string
-	VodEn            string
-	VodStatus        string
-	VodLetter        string
-	VodColor         string
-	VodTag           string
-	VodClass         string
-	VodPic           string
-	PicID            string
-	vodPicThumb      string
-	vodPicSlide      string
-	vodPicScreenshot string
-	vodActor         string
-	vodDirector      string
-	vodWriter        string
-	vodBehind        string
-	vodBlurb         string
-	vodRemarks       string
-	vodPubdate       string
-	VodTotal         string
-	VodSerial        string
-	VodTv            string
-	VodWeekday       string
-	VodArea          string
-	VodLang          string
-	VodYear          string
-	VodVersion       string
-	VodState         string
-	VodAuthor        string
-	VodJumpurl       string
-	VodTpl           string
-	VodTplPlay       string
-	VodTplDown       string
-	VodIsSend        string
-	VodLock          string
-	VodLevel         string
-	VodCopyright     string
-	VodPoints        string
-	VodPointsPlay    string
-	VodPointsDown    string
-	VodHits          string
-	VodHitsDay       string
-	VodHitsWeek      string
-	VodHitsMonth     string
-	VodDuration      string
-	VodUp            string
-	VodDown          string
-	VodScore         string
-	VodScoreAll      string
-	VodScoreNum      string
-	VodTime          string
-	VodTimeAdd       string
-	VodTimeHits      string
-	VodTimeMake      string
-	VodTrysee        string
-	VodDoubanId      string
-	VodDoubanScore   string
-	VodReUrl         string
-	VodReVod         string
-	VodReArt         string
-	VodPwd           string
-	VodPwdUrl        string
-	VodPwdPlay       string
-	VodPwdPlayUrl    string
-	VodPwdDown       string
-	VodPwdDownUrl    string
-	VodContent       string
-	VodPlayFrom      string //"wjyun$$$wjm3u8",
-	VodPlayServer    string //"no$$$no",
-	VodPlayNote      string //$$$
-	VodPlayUrl       string //"DVD$https://v4.cdtlas.com/share/KJhnO2ZC23aiA8NO$$$DVD$https://v4.cdtlas.com/20220212/P4WVmj2q/index.m3u8",
-	VodDownFrom      string
-	VodDownServer    string
-	VodDownNote      string
-	VodDownUrl       string
-	VodPlot          string
-	VodPlotName      string
-	VodPlotDetail    string
-	TypeName         string
-}
-
 type WJVod struct {
-	VodId            int       `json:"vod_id"`
-	TypeId           int       `json:"type_id"`
-	TypeIdParent     int       `json:"type_id_1"`
-	GroupId          int       `json:"group_id"`
-	VodName          string    `json:"vod_name"`
-	VodSub           string    `json:"vod_sub"`
-	VodEn            string    `json:"vod_en"`
-	VodStatus        int       `json:"vod_status"`
-	VodLetter        string    `json:"vod_letter"`
-	VodColor         string    `json:"vod_color"`
-	VodTag           string    `json:"vod_tag"`
-	VodClass         string    `json:"vod_class"`
-	VodPic           string    `json:"vod_pic"`
-	vodPicThumb      string    `json:"vod_pic_thumb"`
-	vodPicSlide      string    `json:"vod_pic_slide"`
-	vodPicScreenshot string    `json:"vod_pic_screenshot"`
-	vodActor         string    `json:"vod_actor"`
-	vodDirector      string    `json:"vod_director"`
-	vodWriter        string    `json:"vod_writer"`
-	vodBehind        string    `json:"vod_behind"`
-	vodBlurb         string    `json:"vod_blurb"`
-	vodRemarks       string    `json:"vod_remarks"`
-	vodPubdate       string    `json:"vod_pubdate"`
-	VodTotal         int       `json:"vod_total"`
-	VodSerial        string    `json:"vod_serial"`
-	VodTv            string    `json:"vod_tv"`
-	VodWeekday       string    `json:"vod_weekday"`
-	VodArea          string    `json:"vod_area"`
-	VodLang          string    `json:"vod_lang"`
-	VodYear          string    `json:"vod_year"`
-	VodVersion       string    `json:"vod_version"`
-	VodState         string    `json:"vod_state"`
-	VodAuthor        string    `json:"vod_author"`
-	VodJumpurl       string    `json:"vod_jumpurl"`
-	VodTpl           string    `json:"vod_tpl"`
-	VodTplPlay       string    `json:"vod_tpl_play"`
-	VodTplDown       string    `json:"vod_tpl_down"`
-	VodIsSend        int       `json:"vod_isend"`
-	VodLock          int       `json:"vod_lock"`
-	VodLevel         int       `json:"vod_level"`
-	VodCopyright     int       `json:"vod_copyright"`
-	VodPoints        int       `json:"vod_points"`
-	VodPointsPlay    int       `json:"vod_points_play"`
-	VodPointsDown    int       `json:"vod_points_down"`
-	VodHits          int       `json:"vod_hits"`
-	VodHitsDay       int       `json:"vod_hits_day"`
-	VodHitsWeek      int       `json:"vod_hits_week"`
-	VodHitsMonth     int       `json:"vod_hits_month"`
-	VodDuration      string    `json:"vod_duration"`
-	VodUp            int       `json:"vod_up"`
-	VodDown          int       `json:"vod_down"`
-	VodScore         string    `json:"vod_score"`
-	VodScoreAll      int       `json:"vod_score_all"`
-	VodScoreNum      int       `json:"vod_score_num"`
-	VodTime          string    `json:"vod_time"`
-	VodTimeAdd       int       `json:"vod_time_add"`
-	VodTimeHits      int       `json:"vod_time_hits"`
-	VodTimeMake      int       `json:"vod_time_make"`
-	VodTrysee        int       `json:"vod_trysee"`
-	VodDoubanId      int       `json:"vod_douban_id"`
-	VodDoubanScore   string    `json:"vod_douban_score"`
-	VodReUrl         string    `json:"vod_reurl"`
-	VodReVod         string    `json:"vod_rel_vod"`
-	VodReArt         string    `json:"vod_rel_art"`
-	VodPwd           string    `json:"vod_pwd"`
-	VodPwdUrl        string    `json:"vod_pwd_url"`
-	VodPwdPlay       string    `json:"vod_pwd_play"`
-	VodPwdPlayUrl    string    `json:"vod_pwd_play_url"`
-	VodPwdDown       string    `json:"vod_pwd_down"`
-	VodPwdDownUrl    string    `json:"vod_pwd_down_url"`
-	VodContent       string    `json:"vod_content"`
-	VodPlayFrom      string    `json:"vod_play_from"`
-	VodPlayServer    string    `json:"vod_play_server"`
-	VodPlayNote      string    `json:"vod_play_note"`
-	VodPlayUrl       string    `json:"vod_play_url"`
-	VodDownFrom      string    `json:"vod_down_from"`
-	VodDownServer    string    `json:"vod_down_server"`
-	VodDownNote      string    `json:"vod_down_note"`
-	VodDownUrl       string    `json:"vod_down_url"`
-	VodPlot          int       `json:"vod_plot"`
-	VodPlotName      string    `json:"vod_plot_name"`
-	VodPlotDetail    string    `json:"vod_plot_detail"`
-	TypeName         string    `json:"type_name"`
-	ImgUrl           string    `json:"img_url"`
+	VodId            int       `bson:"vod_id" json:"vod_id"`
+	TypeId           int       `bson:"type_id" json:"type_id"`
+	TypeIdParent     int       `bson:"type_id_1" json:"type_id_1"`
+	GroupId          int       `bson:"group_id" json:"group_id"`
+	VodName          string    `bson:"vod_name" json:"vod_name"`
+	VodSub           string    `bson:"vod_sub" json:"vod_sub"`
+	VodEn            string    `bson:"vod_en" json:"vod_en"`
+	VodStatus        int       `bson:"vod_status" json:"vod_status"`
+	VodLetter        string    `bson:"vod_letter" json:"vod_letter"`
+	VodColor         string    `bson:"vod_color" json:"vod_color"`
+	VodTag           string    `bson:"vod_tag" json:"vod_tag"`
+	VodClass         string    `bson:"vod_class" json:"vod_class"`
+	VodPic           string    `bson:"vod_pic" json:"vod_pic"`
+	vodPicThumb      string    `bson:"vod_pic_thumb" json:"vod_pic_thumb"`
+	vodPicSlide      string    `bson:"vod_pic_slide" json:"vod_pic_slide"`
+	vodPicScreenshot string    `bson:"vod_pic_screenshot" json:"vod_pic_screenshot"`
+	vodActor         string    `bson:"vod_actor" json:"vod_actor"`
+	vodDirector      string    `bson:"vod_director" json:"vod_director"`
+	vodWriter        string    `bson:"vod_writer" json:"vod_writer"`
+	vodBehind        string    `bson:"vod_behind" json:"vod_behind"`
+	vodBlurb         string    `bson:"vod_blurb" json:"vod_blurb"`
+	vodRemarks       string    `bson:"vod_remarks" json:"vod_remarks"`
+	vodPubdate       string    `bson:"vod_pubdate" json:"vod_pubdate"`
+	VodTotal         int       `bson:"vod_total" json:"vod_total"`
+	VodSerial        string    `bson:"vod_serial" json:"vod_serial"`
+	VodTv            string    `bson:"vod_tv" json:"vod_tv"`
+	VodWeekday       string    `bson:"vod_weekday" json:"vod_weekday"`
+	VodArea          string    `bson:"vod_area" json:"vod_area"`
+	VodLang          string    `bson:"vod_lang" json:"vod_lang"`
+	VodYear          string    `bson:"vod_year" json:"vod_year"`
+	VodVersion       string    `bson:"vod_version" json:"vod_version"`
+	VodState         string    `bson:"vod_state" json:"vod_state"`
+	VodAuthor        string    `bson:"vod_author" json:"vod_author"`
+	VodJumpurl       string    `bson:"vod_jumpurl" json:"vod_jumpurl"`
+	VodTpl           string    `bson:"vod_tpl" json:"vod_tpl"`
+	VodTplPlay       string    `bson:"vod_tpl_play" json:"vod_tpl_play"`
+	VodTplDown       string    `bson:"vod_tpl_down" json:"vod_tpl_down"`
+	VodIsSend        int       `bson:"vod_isend" json:"vod_isend"`
+	VodLock          int       `bson:"vod_lock" json:"vod_lock"`
+	VodLevel         int       `bson:"vod_level" json:"vod_level"`
+	VodCopyright     int       `bson:"vod_copyright" json:"vod_copyright"`
+	VodPoints        int       `bson:"vod_points" json:"vod_points"`
+	VodPointsPlay    int       `bson:"vod_points_play" json:"vod_points_play"`
+	VodPointsDown    int       `bson:"vod_points_down" json:"vod_points_down"`
+	VodHits          int       `bson:"vod_hits" json:"vod_hits"`
+	VodHitsDay       int       `bson:"vod_hits_day" json:"vod_hits_day"`
+	VodHitsWeek      int       `bson:"vod_hits_week" json:"vod_hits_week"`
+	VodHitsMonth     int       `bson:"vod_hits_month" json:"vod_hits_month"`
+	VodDuration      string    `bson:"vod_duration" json:"vod_duration"`
+	VodUp            int       `bson:"vod_up" json:"vod_up"`
+	VodDown          int       `bson:"vod_down" json:"vod_down"`
+	VodScore         string    `bson:"vod_score" json:"vod_score"`
+	VodScoreAll      int       `bson:"vod_score_all" json:"vod_score_all"`
+	VodScoreNum      int       `bson:"vod_score_num" json:"vod_score_num"`
+	VodTime          string    `bson:"vod_time" json:"vod_time"`
+	VodTimeAdd       int       `bson:"vod_time_add" json:"vod_time_add"`
+	VodTimeHits      int       `bson:"vod_time_hits" json:"vod_time_hits"`
+	VodTimeMake      int       `bson:"vod_time_make" json:"vod_time_make"`
+	VodTrysee        int       `bson:"vod_trysee" json:"vod_trysee"`
+	VodDoubanId      int       `bson:"vod_douban_id" json:"vod_douban_id"`
+	VodDoubanScore   string    `bson:"vod_douban_score" json:"vod_douban_score"`
+	VodReUrl         string    `bson:"vod_reurl" json:"vod_reurl"`
+	VodReVod         string    `bson:"vod_rel_vod" json:"vod_rel_vod"`
+	VodReArt         string    `bson:"vod_rel_art" json:"vod_rel_art"`
+	VodPwd           string    `bson:"vod_pwd" json:"vod_pwd"`
+	VodPwdUrl        string    `bson:"vod_pwd_url" json:"vod_pwd_url"`
+	VodPwdPlay       string    `bson:"vod_pwd_play" json:"vod_pwd_play"`
+	VodPwdPlayUrl    string    `bson:"vod_pwd_play_url" json:"vod_pwd_play_url"`
+	VodPwdDown       string    `bson:"vod_pwd_down" json:"vod_pwd_down"`
+	VodPwdDownUrl    string    `bson:"vod_pwd_down_url" json:"vod_pwd_down_url"`
+	VodContent       string    `bson:"vod_content" json:"vod_content"`
+	VodPlayFrom      string    `bson:"vod_play_from" json:"vod_play_from"`
+	VodPlayServer    string    `bson:"vod_play_server" json:"vod_play_server"`
+	VodPlayNote      string    `bson:"vod_play_note" json:"vod_play_note"`
+	VodPlayUrl       string    `bson:"vod_play_url" json:"vod_play_url"`
+	VodDownFrom      string    `bson:"vod_down_from" json:"vod_down_from"`
+	VodDownServer    string    `bson:"vod_down_server" json:"vod_down_server"`
+	VodDownNote      string    `bson:"vod_down_note" json:"vod_down_note"`
+	VodDownUrl       string    `bson:"vod_down_url" json:"vod_down_url"`
+	VodPlot          int       `bson:"vod_plot" json:"vod_plot"`
+	VodPlotName      string    `bson:"vod_plot_name" json:"vod_plot_name"`
+	VodPlotDetail    string    `bson:"vod_plot_detail" json:"vod_plot_detail"`
+	TypeName         string    `bson:"type_name" json:"type_name"`
+	ImgUrl           string    `bson:"img_url" json:"img_url""`
 	UpdatedTime      time.Time `bson:"updated_time"`
 	CreatedTime      time.Time `bson:"created_time"`
 }
